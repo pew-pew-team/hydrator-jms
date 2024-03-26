f5130d39793e991e665181b6b7bcb4566cc00165
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-03-27 01:05:20 +0300

    Add handlers and subscribers

--- src/Builder.php
+++ src/Builder.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace PewPew\Hydrator\JMS;
 
+use JMS\Serializer\EventDispatcher\EventDispatcher;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\Handler\HandlerRegistry;
+use JMS\Serializer\Handler\SubscribingHandlerInterface;
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
 use JMS\Serializer\Serializer;
 use JMS\Serializer\SerializerBuilder;
@@ -29,6 +33,10 @@ final readonly class Builder
     /**
      * @param array<class-string, non-empty-string> $configs Sets a map of
      *        namespace prefixes to directories.
+     * @param iterable<array-key, SubscribingHandlerInterface> $handlers List
+     *        of subscribing handlers.
+     * @param iterable<array-key, EventSubscriberInterface> $subscribers List
+     *        of event subscribers.
      *
      * @psalm-suppress InternalClass
      * @psalm-suppress TooManyArguments
@@ -36,11 +44,24 @@ final readonly class Builder
     public static function create(
         array $configs = [],
         bool $debug = false,
+        iterable $handlers = [],
+        iterable $subscribers = [],
         CacheItemPoolInterface $cache = null,
     ): self {
         $builder = SerializerBuilder::create()
             ->setDebug($debug)
-            ->setMetadataDirs($configs);
+            ->addDefaultHandlers()
+            ->setMetadataDirs($configs)
+            ->configureHandlers(static function (HandlerRegistry $registry) use ($handlers): void {
+                foreach ($handlers as $handler) {
+                    $registry->registerSubscribingHandler($handler);
+                }
+            })
+            ->configureListeners(static function (EventDispatcher $dispatcher) use ($subscribers): void {
+                foreach ($subscribers as $subscriber) {
+                    $dispatcher->addSubscriber($subscriber);
+                }
+            });
 
         if ($cache !== null) {
             $builder->setMetadataCache(new MetadataPsrCacheAdapter('jms_hydrator', $cache));
